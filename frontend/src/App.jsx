f5bfc30c2522ea15ff3fc6808ed778449631fdb1
Shivam Aggarwal <shiaggarwal@microsoft.com>
2025-04-04 20:46:35 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,9 +20,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">
-        📈 Real-Time Stock Price
-      </h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">📈 RTSP</h1>
       <h2>Last Update: {lastUpdated}</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {Object.entries(stockData).map(([company, { price, timestamp }]) => (
@@ -32,9 +30,9 @@ export default function App() {
           >
             <h2 className="text-xl font-semibold mb-2">{company}</h2>
             <p className="text-green-600 text-lg font-mono">${price}</p>
-            <p className="text-gray-400 text-sm mt-1">
+            {/* <p className="text-gray-400 text-sm mt-1">
               {new Date(timestamp).toLocaleTimeString()}
-            </p>
+            </p> */}
           </div>
         ))}
       </div>
